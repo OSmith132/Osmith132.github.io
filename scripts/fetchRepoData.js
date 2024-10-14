99669914839530834172450749dbff124c328689
Oliver Smith <osmith04@btinternet.com>
2024-10-15 00:13:35 +0100

    Format correctly

--- scripts/fetchRepoData.js
+++ scripts/fetchRepoData.js
@@ -1,32 +1,34 @@
-
-
 async function fetchRepoData(repoOwner, repoName) {
   // Fetch repository information
   fetch(`https://api.github.com/repos/${repoOwner}/${repoName}`)
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('description').innerText = `${data.description || '...'}`;
-      document.getElementById('stars').innerText = data.stargazers_count || '...';
-      document.getElementById('forks').innerText = data.forks_count || '...';
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("description").innerText = `${
+        data.description || "..."
+      }`;
+      document.getElementById("stars").innerText =
+        data.stargazers_count || "...";
+      document.getElementById("forks").innerText = data.forks_count || "...";
     })
-    .catch(error => {
-      console.error('Error fetching repository data:', error);
+    .catch((error) => {
+      console.error("Error fetching repository data:", error);
     });
 
   // Fetch user profile data
   fetch(`https://api.github.com/users/${repoOwner}`)
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
-        throw new Error('Profile not found');
+        throw new Error("Profile not found");
       }
       return response.json();
     })
-    .then(data => {
-      document.getElementById('profile-pic').src = data.avatar_url;
+    .then((data) => {
+      document.getElementById("profile-pic").src = data.avatar_url;
     })
-    .catch(error => {
-      console.error('Error fetching user profile:', error);
-      document.getElementById('profile-pic').src = 'assets/icons/warning-icon.png';
+    .catch((error) => {
+      console.error("Error fetching user profile:", error);
+      document.getElementById("profile-pic").src =
+        "assets/icons/warning-icon.png";
     });
 
   // Fetch total commits
@@ -36,11 +38,13 @@ async function fetchRepoData(repoOwner, repoName) {
     const perPage = 100;
 
     while (true) {
-      const response = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/commits?per_page=${perPage}&page=${page}`);
+      const response = await fetch(
+        `https://api.github.com/repos/${repoOwner}/${repoName}/commits?per_page=${perPage}&page=${page}`
+      );
 
       if (!response.ok) {
-        console.error('Error fetching commits data:', response.statusText);
-        document.getElementById('commits').innerText = '...';
+        console.error("Error fetching commits data:", response.statusText);
+        document.getElementById("commits").innerText = "...";
         return;
       }
 
@@ -54,33 +58,32 @@ async function fetchRepoData(repoOwner, repoName) {
       page++;
     }
 
-    document.getElementById('commits').innerText = totalCommits;
+    document.getElementById("commits").innerText = totalCommits;
   }
 
   fetchTotalCommits();
 
   // Fetch languages used in the repository
   fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/languages`)
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
-        throw new Error('Repo cannot be reached');
+        throw new Error("Repo cannot be reached");
       }
       return response.json();
     })
-    .then(data => {
+    .then((data) => {
       // Convert the object to an array of entries and sort it by usage
       const sortedLanguages = Object.entries(data)
         .sort((a, b) => b[1] - a[1]) // Sort by usage
         .slice(0, 3); // Get the top 3
 
       // Map to a string format
-      const languages = sortedLanguages.map(lang => lang[0]).join(', ');
-      document.getElementById('languages').innerText = languages || 'No languages found';
+      const languages = sortedLanguages.map((lang) => lang[0]).join(", ");
+      document.getElementById("languages").innerText =
+        languages || "No languages found";
     })
-    .catch(error => {
-      console.error('Error fetching languages data:', error);
-      document.getElementById('languages').innerText = '...';
+    .catch((error) => {
+      console.error("Error fetching languages data:", error);
+      document.getElementById("languages").innerText = "...";
     });
 }
-
-
--- scripts/main.js
+++ scripts/main.js
@@ -1,152 +1,137 @@
 // Set colour mode based on local storage (defaults to light mode)
-document.addEventListener("DOMContentLoaded", function() {
-
-    if (localStorage.getItem("colourMode") === "dark") {
-        setColourMode(false);
-    } else {
-        setColourMode(true);
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  if (localStorage.getItem("colourMode") === "dark") {
+    setColourMode(false);
+  } else {
+    setColourMode(true);
+  }
 });
-
 
 // Set dark/light mode
 function toggleColourMode() {
-    const currentMode = localStorage.getItem("colourMode");
-    if (currentMode === "dark") {
-        setColourMode(true); // Switch to light mode
-    } else {
-        setColourMode(false); // Switch to dark mode
-    }
+  const currentMode = localStorage.getItem("colourMode");
+  if (currentMode === "dark") {
+    setColourMode(true); // Switch to light mode
+  } else {
+    setColourMode(false); // Switch to dark mode
+  }
 }
 
 // Set dark/light mode
 function setColourMode(lightMode) {
-    const root = document.querySelector(":root");
-    const headerIcon = document.getElementById("header-icon");
-    const colourModeButton = document.getElementById("colour-mode-button");
+  const root = document.querySelector(":root");
+  const headerIcon = document.getElementById("header-icon");
+  const colourModeButton = document.getElementById("colour-mode-button");
 
-    if (!lightMode) {
+  if (!lightMode) {
+    // ENABLE DARK MODE
+    root.style.setProperty("--background", "#babeba");
+    //  root.style.setProperty("--header", "#293535");
+    root.style.setProperty("--text", "#424242");
+    // root.style.setProperty("--alternate-text", "white");
+    // root.style.setProperty("--header-button", "#e7d8c9");
+    // root.style.setProperty("--header-light", "#879183");
+    root.style.setProperty("--showcase-box", "#d3cfca"); // #ddd8d3
+    root.style.setProperty("--showcase-box-element", "rgba(200,200,200, 0.3)");
+    root.style.setProperty(
+      "--showcase-box-element-hover",
+      "rgba(150,150,150, 0.3)"
+    );
 
-        // ENABLE DARK MODE
-        root.style.setProperty("--background", "#babeba");
-        //  root.style.setProperty("--header", "#293535");
-        root.style.setProperty("--text", "#424242");
-        // root.style.setProperty("--alternate-text", "white");
-        // root.style.setProperty("--header-button", "#e7d8c9");
-        // root.style.setProperty("--header-light", "#879183");
-        root.style.setProperty("--showcase-box", "#d3cfca"); // #ddd8d3
-        root.style.setProperty("--showcase-box-element", "rgba(200,200,200, 0.3)");
-        root.style.setProperty("--showcase-box-element-hover", "rgba(150,150,150, 0.3)");
-        
-
-        // Update header icon
-        if (headerIcon) {
-            headerIcon.style.filter = null;
-        }
-
-        // Update toggle button
-        colourModeButton.setAttribute("title", "Enable Light Mode");
-        colourModeButton.innerHTML = "<i class='fa-solid fa-sun'></i>";
-
-        // Update local storage
-        localStorage.setItem("colourMode", "dark");
-        
-
-
-    } else {
-
-        // ENABLE LIGHT MODE
-        root.style.setProperty("--background", "#f5ede4");
-        // root.style.setProperty("--header", "#546666");
-        root.style.setProperty("--text", "#535353");
-        // root.style.setProperty("--alternate-text", "white");
-        // root.style.setProperty("--header-button", "#e7d8c9");
-        // root.style.setProperty("--header-light", "#879183");
-        root.style.setProperty("--showcase-box", "#f5efe9");
-        root.style.setProperty("--showcase-box-element", "rgba(200,200,200, 0.2)");
-        root.style.setProperty("--showcase-box-element-hover", "rgba(150,150,150, 0.3)");
-        
-        
-    
-
-        // Update header icon
-        if (headerIcon) {
-            headerIcon.style.filter = "invert(1)";
-        }
-
-        // Update toggle button
-        colourModeButton.setAttribute("title", "Enable Dark Mode");
-        colourModeButton.innerHTML = "<i class='fa-solid fa-moon'></i>";
-
-        // Update local storage
-        localStorage.setItem("colourMode", "light");
+    // Update header icon
+    if (headerIcon) {
+      headerIcon.style.filter = null;
     }
+
+    // Update toggle button
+    colourModeButton.setAttribute("title", "Enable Light Mode");
+    colourModeButton.innerHTML = "<i class='fa-solid fa-sun'></i>";
+
+    // Update local storage
+    localStorage.setItem("colourMode", "dark");
+  } else {
+    // ENABLE LIGHT MODE
+    root.style.setProperty("--background", "#f5ede4");
+    // root.style.setProperty("--header", "#546666");
+    root.style.setProperty("--text", "#535353");
+    // root.style.setProperty("--alternate-text", "white");
+    // root.style.setProperty("--header-button", "#e7d8c9");
+    // root.style.setProperty("--header-light", "#879183");
+    root.style.setProperty("--showcase-box", "#f5efe9");
+    root.style.setProperty("--showcase-box-element", "rgba(200,200,200, 0.2)");
+    root.style.setProperty(
+      "--showcase-box-element-hover",
+      "rgba(150,150,150, 0.3)"
+    );
+
+    // Update header icon
+    if (headerIcon) {
+      headerIcon.style.filter = "invert(1)";
+    }
+
+    // Update toggle button
+    colourModeButton.setAttribute("title", "Enable Dark Mode");
+    colourModeButton.innerHTML = "<i class='fa-solid fa-moon'></i>";
+
+    // Update local storage
+    localStorage.setItem("colourMode", "light");
+  }
 }
-
-
-
-
 
 // For making table rows with an href clickable
 function clickableRow() {
-    const rows = document.querySelectorAll(".clickable-row");
-    rows.forEach(row => {
-        row.addEventListener("click", () => {
-            window.location.href = row.dataset.href;
-        });
+  const rows = document.querySelectorAll(".clickable-row");
+  rows.forEach((row) => {
+    row.addEventListener("click", () => {
+      window.location.href = row.dataset.href;
     });
+  });
 }
 
 //Open form
 function openForm(formID) {
-    document.getElementById(formID).style.visibility = "visible"
-    document.getElementById(formID).style.opacity = 1;
+  document.getElementById(formID).style.visibility = "visible";
+  document.getElementById(formID).style.opacity = 1;
 }
 
 //Close form
 function closeForm(formID) {
-    setTimeout(function() {
-        document.getElementById(formID).style.visibility = "hidden";
-    }, 300);
-    document.getElementById(formID).style.opacity = 0;
+  setTimeout(function () {
+    document.getElementById(formID).style.visibility = "hidden";
+  }, 300);
+  document.getElementById(formID).style.opacity = 0;
 }
 
 // Show/hide characters in password input
 function togglePasswordVisibility(passwordID) {
-    const element = document.getElementById(passwordID);
+  const element = document.getElementById(passwordID);
 
-    if (element.type === "password") {
-        element.type = "text";
-    } else {
-        element.type = "password";
-    }
+  if (element.type === "password") {
+    element.type = "text";
+  } else {
+    element.type = "password";
+  }
 }
 
+document.querySelector(".toggleButton").addEventListener("click", function () {
+  const pdfViewer = document.querySelector(".pdfViewer");
+  const arrow = document.querySelector(".arrow");
+  const toggleButton = document.querySelector(".toggleButton");
 
-document.querySelector('.toggleButton').addEventListener('click', function() {
-    const pdfViewer = document.querySelector('.pdfViewer');
-    const arrow = document.querySelector('.arrow');
-    const toggleButton = document.querySelector('.toggleButton');
-
-    if (pdfViewer.classList.contains('active')) {
-        pdfViewer.classList.remove('active');
-        arrow.classList.remove('rotate'); // Reset arrow direction
-        // Set border radius for hidden state
-        toggleButton.style.borderBottomLeftRadius = '1vh';
-        toggleButton.style.borderBottomRightRadius = '1vh';
-    } else {
-        pdfViewer.classList.add('active');
-        arrow.classList.add('rotate'); // Rotate arrow upwards
-        // Set border radius for visible state
-        toggleButton.style.borderBottomLeftRadius = '0';
-        toggleButton.style.borderBottomRightRadius = '0';
-    }
+  if (pdfViewer.classList.contains("active")) {
+    pdfViewer.classList.remove("active");
+    arrow.classList.remove("rotate"); // Reset arrow direction
+    // Set border radius for hidden state
+    toggleButton.style.borderBottomLeftRadius = "1vh";
+    toggleButton.style.borderBottomRightRadius = "1vh";
+  } else {
+    pdfViewer.classList.add("active");
+    arrow.classList.add("rotate"); // Rotate arrow upwards
+    // Set border radius for visible state
+    toggleButton.style.borderBottomLeftRadius = "0";
+    toggleButton.style.borderBottomRightRadius = "0";
+  }
 });
-
-
-
-
-
 
 const cardsContainer = document.querySelector(".container");
 
